496881a16aee4d3993ab4fe7e56dd6f61033b88f
nihad1213 <nihad.nemetli@gmail.com>
2024-05-14 22:50:46 +0400

    Task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for digit in range(0, 100):
     if digit == 99:
-        print("{}".format(number))
+        print("{}".format(digit))
     else:
-        print("{:02}".format(number), end=', ')
+        print("{:02}".format(digit), end=', ')
