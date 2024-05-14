840d1321a208c53758cff6532be67aa1a6fde412
nihad1213 <nihad.nemetli@gmail.com>
2024-05-14 23:05:38 +0400

    Task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for digit1 in range(0, 9):
+for digit1 in range(0, 8):
     for digit2 in range(digit1, 10):
         if digit1 != digit2:
             print("{:d}{:d}, ".format(digit1, digit2), end=' ')
+print("89")
