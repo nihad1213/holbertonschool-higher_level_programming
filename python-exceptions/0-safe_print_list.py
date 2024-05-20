a1568b1ee1d2bcae23c87c443232e24707dfc4e6
root <nihad.nemetli@gmail.com>
2024-05-20 11:54:29 +0000

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,8 +7,9 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{:d}".format(my_list[i]), end="")
             i += 1
-    except:
+    except IndexError:
         pass
-    
+
     print()
     return i
+
