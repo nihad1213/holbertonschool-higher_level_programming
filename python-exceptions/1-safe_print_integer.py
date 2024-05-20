bf95f2042e2c8380b6880a7d217f75b427ff5be7
root <nihad.nemetli@gmail.com>
2024-05-20 12:01:29 +0000

    Task 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except Error:
+    except (ValueError, TypeError):
         return False
     else:
         return True
