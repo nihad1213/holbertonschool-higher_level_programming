e63fa507850a2c1c45099a0bbd99a29bedbf111a
root <nihad.nemetli@gmail.com>
2024-05-20 09:46:57 +0000

    Task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple_a + tuple_b
+    list1 = list(tuple_a)
+    list2 = list(tuple_b)
+
+    for i in range(2):
+        list1.append(0)
+        list2.append(0)
+
+    sum_1 = list1[0] + list2[0]
+    sum_2 = list1[1] + list2[1]
+
+    tuple_c = (sum_1, sum_2)
+
+    return tuple_c
+
