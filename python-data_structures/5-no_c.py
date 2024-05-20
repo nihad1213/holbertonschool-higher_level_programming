a3ff90c269bf889289cc72546bdff0332c3026dc
root <nihad.nemetli@gmail.com>
2024-05-20 08:37:25 +0000

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_string = ''
     for letters in my_string:
-        if letters != 'C' or letters != 'c':
+        if letters != "C" or letters != "c":
             new_string += letters
     return new_string
