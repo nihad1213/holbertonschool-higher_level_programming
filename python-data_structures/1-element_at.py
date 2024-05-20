656a4dd9e1c6281e99f207fe0fe32186fe3459f4
root <nihad.nemetli@gmail.com>
2024-05-20 08:08:00 +0000

    Task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
-            return None
-        if idx > len(my_list):
-            return None
-        print("Element at index {:d} is {:d}".format(idx, my_list[i]))
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    return my_list[idx]
