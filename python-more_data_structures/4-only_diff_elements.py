2c1d559061308f51308b17b497e94d3786f8f297
root <nihad.nemetli@gmail.com>
2024-05-21 08:31:57 +0000

    Task 4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    final_list = set_1 + set_2
+    final_final_list = set(final_list)
+    final_final_final_list = list(final_final_list)
+
+    return final_final_final_list
