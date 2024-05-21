1b25562c13c43092d27bbaf7b2985427d61fe507
root <nihad.nemetli@gmail.com>
2024-05-21 08:01:56 +0000

    Task 1

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,9 @@
 def uniq_add(my_list=[]):
     new_list = set(my_list)
     new_new_list = list(new_list)
-
+    
+    if len(new_new_lsit) == 0:
+        return 0
     sum = 0
 
     for i in range(0, len(new_new_list)):
