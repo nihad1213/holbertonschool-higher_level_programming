e298e5bb24a8e7a3a3eb7c76ae5985bad9251425
root <nihad.nemetli@gmail.com>
2024-05-21 08:16:43 +0000

    Task 3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_element = {}
+
+    for i in set_1:
+        if i in common_element:
+            common_element[i] += 1
+        else:
+            common_element[i] = 1
+    
+    for i in set_2:
+        if i in common_element:
+            common_element[i] += 1
+        else:
+            common_element[i] = 1
+
+    common_element_ = [i for i in common_element if common_element[i] == 2]
+    if len(common_element_) > 0:
+        return common_element_
+    else:
+        #return nothing
