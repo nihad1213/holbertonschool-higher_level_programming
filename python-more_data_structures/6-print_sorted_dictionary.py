dc60f359e73b7cc5b18fa6393c4f832256fb3557
root <nihad.nemetli@gmail.com>
2024-05-21 09:03:17 +0000

    Task

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    dictionary = list(a_dictionary)
+    dictionary.sort()
+    sorted_dic = for i in dictionary
+
+    return sorted_dic
