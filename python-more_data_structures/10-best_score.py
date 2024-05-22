4c340cefb4935243ffd0d3a7fa0141ad358472c3
root <nihad.nemetli@gmail.com>
2024-05-22 08:59:10 +0000

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
+        max_val = max(a_dictionary.values())
+        return max_val
     else:
         return None
