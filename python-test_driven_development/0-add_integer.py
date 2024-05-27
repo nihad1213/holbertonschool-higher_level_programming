06b07301ebed61ff3f4c00969cf7089b9181bcbf
root <nihad.nemetli@gmail.com>
2024-05-27 08:06:03 +0000

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" 
+    Add integer
+"""
 
 def add_integer(a, b=98):
+    """Add integer"""
     if type(a) is not float and type(a) is not int:
         raise TypeError("a must be an integer")
     if type(b) is not float and type(b) is not int:
