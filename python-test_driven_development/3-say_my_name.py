d3bcaa42c018dbbffaedf543602ec7bcbf1b761f
root <nihad.nemetli@gmail.com>
2024-05-27 08:58:54 +0000

    Task 2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    SOAD - Aerials
+"""
+
+
+def say_my_name(first_name, last_name""):
+    """ say_my_name """
+    if type(first_name) is not str and type(last_name) is not str:
+        raise TypeError("first_name or last_name must be string")
+        print(f"My name is {:s} {:s}".format(first_name, last_name))
