30cb40ba7c0e5bc52a5ab1c23fa2987a02246e82
root <nihad.nemetli@gmail.com>
2024-05-27 09:24:18 +0000

    Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,8 @@
 
 def say_my_name(first_name, last_name=""):
     """ say_my_name """
-    if type(first_name) is not str and type(last_name) is not str:
-        raise TypeError("first_name or last_name must be string")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be string")
     print("My name is {:s} {:s}".format(first_name, last_name))
