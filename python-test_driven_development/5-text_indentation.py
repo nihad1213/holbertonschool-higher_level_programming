cbd8c1b49a45032658289aff0d2f6506f122a0cf
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-28 08:16:36 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 """
-    Balabey - Sessiz geceler
+    A
 """
-def text_indentation(text):
-    """Function"""
-    if type(text) is not str:
-        raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+
+def text_indentation(text):
+    """ insert doble jump line after . : or ? """
+    str_error = "text must be a string"
+    new_text = ""
+    flag = False
+    if type(text) is not str:
+        raise TypeError(str_error)
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace("? ", "?")
+    for char in new_text:
+        if char in [".", "?", ":"]:
+            print(char)
+            print()
+            flag = True
+        else:
+            if flag is False:
+                print(char, end="")
+            else:
+                if char != " ":
+                    print(char, end="")
+                    flag = False
