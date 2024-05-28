e4c0c3f23d1ae1b1fe4ad736d3669013c63bd86b
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-28 21:34:29 +0400

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
-"""Task 1"""
+"""
+    Rectangle
+"""
 
 
-class Rectangle:
-    """Rectangle class"""
+class Rectangle: 
+    """
+        Rectangle
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError('height must be >= 0')
-        self._height = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __str__(self):
+        total = ""
+        if self.__height == 0 or self.width == 0:
+            return total
+        for i in range(self.__height):
+            total += ("#" * self.__width)
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
