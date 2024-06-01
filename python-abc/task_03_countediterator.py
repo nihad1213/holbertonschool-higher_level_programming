5d15de40d3a7b75b3824b19d069961ff86d9a15a
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-06-02 01:59:29 +0400

    Create task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class CountedIterator:
+"""Class"""
+
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration("No more items to iterate")
