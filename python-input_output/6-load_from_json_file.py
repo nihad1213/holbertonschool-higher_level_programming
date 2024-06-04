b9526f36ef25c8de4e5d04ad5bf512fbfdb8dad4
root <nihad.nemetli@gmail.com>
2024-06-04 06:14:56 +0000

    Task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 def load_from_json_file(filename):
     """Pantera - Walk"""
     with open(filename, 'r', encoding="utf-8") as file:
-            object_ = json.load(file)
-            return object_
+            object_o = json.load(file)
+            return object_o
