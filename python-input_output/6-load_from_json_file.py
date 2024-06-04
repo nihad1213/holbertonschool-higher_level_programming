938e96de2f8aee5d1c1463d42a2214eba8f09e98
root <nihad.nemetli@gmail.com>
2024-06-04 06:12:53 +0000

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Holy Wars"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Pantera - Walk"""
+    with open(filename, 'r', encoding="utf-8" as file:
+            object_ = json.load(file)
+            return object_
