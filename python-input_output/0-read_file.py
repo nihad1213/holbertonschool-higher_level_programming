c5d467dc8cd7cd5c9acd4713b7552a4dda96d0de
root <nihad.nemetli@gmail.com>
2024-06-04 04:21:51 +0000

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """function read and print file"""
     with open(filename, "r", encoding="UTF-8") as file:
         print(file.read(), end="")
