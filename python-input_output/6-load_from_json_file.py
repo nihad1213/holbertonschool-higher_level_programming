d8d194c43b6bde20f3bc6a4b25694dfa2d86710d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-06-08 21:13:15 +0400

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Holy Wars"""
 
-
 import json
-
 
 def load_from_json_file(filename):
     """Pantera - Walk"""
     with open(filename, 'r', encoding="utf-8") as file:
-            object_o = json.load(file)
-            return object_o
+        object_o = json.load(file)
+        return object_o
