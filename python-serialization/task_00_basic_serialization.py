4001c9c6fc3a874a8b64aecaf08b2c72297858d3
nihad1213 <nihad.nemetli@gmail.com>
2024-06-08 21:33:56 +0400

    Task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Task"""
-
-
+"""Module containing basic serialization"""
 import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """Def"""
-    with open(filename, 'w') as f:
-        json.dump(data, f)
-    pass
+    """Function to serialize and save to the specified file"""
+
+    # Serializing the data
+    content = json.dumps(data)
+
+    # Writing serialized data to the file
+    with open(filename, 'w') as file:
+        file.write(content)
+
 
 def load_and_deserialize(filename):
-    """Def"""
-    with open(filename. r) as f:
-        return json.load(f)
-    pass
+    """Function to load and deserialize"""
+
+    # Reading file content for deserialization
+    with open(filename, 'r', encoding='UTF-8') as file:
+        content = file.read()
+
+    # Returning deserialized data
+    return json.loads(content)
