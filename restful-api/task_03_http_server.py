d14191668cddaae479c0520bc31d4e95eddc5d94
nihad1213 <nihad.nemetli@gmail.com>
2024-06-09 22:55:25 +0400

    Task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import http.server
+from http.server import HTTPServer
+import json
+
+
+class test(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+
+def run(server_class=HTTPServer, handler_class=test, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running on port {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
+
