5df5967b73b31726f4eecb1c64240bd3e9b19b27
root <nihad.nemetli@gmail.com>
2024-06-25 05:40:46 +0000

    Task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
 -- States Table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
 
-CREATE TABLE IF NOT EXISTS `states`(
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL,,
 	UNIQUE(id)	
