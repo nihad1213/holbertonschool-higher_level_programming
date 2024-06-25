17e0200ea04466b3499a3e24d30e1f9aa87b8fcd
nihad1213 <nihad.nemetli@gmail.com>
2024-06-25 21:08:51 +0400

    Task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- Cities
-CREATE DATABASE `hbtn_0d_usa`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
-CREATE TABLE `hbtn_0d_usa`.`cities` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	`id` 		INT 		NOT NULL AUTO_INCREMENT,
 	`state_id` 	INT 		NOT NULL,
 	`name` 		VARCHAR(256) 	NOT NULL,
