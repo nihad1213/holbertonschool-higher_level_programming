5520a329e7573dd1cdc70f75a7f0f5eb04c18881
nihad1213 <nihad.nemetli@gmail.com>
2024-06-26 22:32:15 +0400

    Task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """List for state.id"""
-
 import MySQLdb
 from sys import argv
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
