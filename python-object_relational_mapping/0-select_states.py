8cfda609fe44e110d04e267a2cacbe4abff242c8
nihad1213 <nihad.nemetli@gmail.com>
2024-06-26 22:32:58 +0400

    Task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""List for state.id"""
+"""lists all for state.id"""
+
 import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    numberOfRows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for a in range(numberOfRows):
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
         print(cur.fetchone())
