51696cfa5c9526fe1a387983b74dace03cb69fce
nihad1213 <nihad.nemetli@gmail.com>
2024-07-15 15:03:58 +0400

    a

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -3,29 +3,13 @@ import json
 
 app = Flask(__name__)
 
-# Route for index.html file
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-# Route for about.html file
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-# Route for contact.html file
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-# Route for items.html file
-@app.route('/items', strict_slashes=True)
-def items():
+@app.route('/items')
+def show_items():
     with open('items.json', 'r') as f:
         items_data = json.load(f)
-    items_list = items_data['items']
-    return render_template('items.html', itemss=items_list)
+    
+    items = items_data['items']
+    return render_template('items.html', items=items)
 
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
