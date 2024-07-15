5aeb88e14f853c559ae78ca611aad4f9f100ae8e
nihad1213 <nihad.nemetli@gmail.com>
2024-07-15 14:27:14 +0400

    Task 2

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -2,21 +2,6 @@ from flask import Flask, render_template
 import json
 app = Flask(__name__)
 
-# Route for index.html file
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-# Route for about.html file
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-# Route for contact.html file
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
 # Route for items.html file
 @app.route('/items')
 def items():
